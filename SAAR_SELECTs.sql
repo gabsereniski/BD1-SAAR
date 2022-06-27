562c49d3acb05e0511a077699448453925aa16ad
Gabriela Paola Sereniski <gabsereniski@gmail.com>
2022-06-27 18:00:46 -0300

    Update SAAR_SELECTs.sql

--- SAAR_SELECTs.sql
+++ SAAR_SELECTs.sql
@@ -4,9 +4,9 @@ from Funcionario F, Funcion_garcom G
 where F.id = G.id
 and exists (
 	select id_garcom
-    from Garcom_atende_Mesa A
-    where A.gorjeta is not null
-    and A.id_garcom = F.id
+	from Garcom_atende_Mesa A
+	where A.gorjeta is not null
+	and A.id_garcom = F.id
 );
 
 -- 2. Selecione as comandas que pediram apenas porções
@@ -14,11 +14,11 @@ select C.numero
 from Comanda C
 where C.numero not in (
 	select P.num_comanda
-    from Pedido P, Pedido_tem_Item_com_Tamanho PIT, Item_do_menu I, Categoria C 
-    where P.numero = PIT.num_pedido
-    and PIT.id_item = I.id
-    and I.categoria = C.nome
-    and C.nome != 'Porções'
+	from Pedido P, Pedido_tem_Item_com_Tamanho PIT, Item_do_menu I, Categoria C 
+	where P.numero = PIT.num_pedido
+	and PIT.id_item = I.id
+	and I.categoria = C.nome
+	and C.nome != 'Porções'
 );
 
 -- 3. Selecione as comandas que pediram apenas porções e as que pediram apenas cervejas artesanais
@@ -26,22 +26,22 @@ select C.numero
 from Comanda C
 where C.numero not in (
 	select P.num_comanda
-    from Pedido P, Pedido_tem_Item_com_Tamanho PIT, Item_do_menu I, Categoria C 
-    where P.numero = PIT.num_pedido
-    and PIT.id_item = I.id
+	from Pedido P, Pedido_tem_Item_com_Tamanho PIT, Item_do_menu I, Categoria C 
+	where P.numero = PIT.num_pedido
+	and PIT.id_item = I.id
 	and I.categoria = C.nome
-    and C.nome != 'Porções'
+	and C.nome != 'Porções'
 )
 union
 select C.numero
 from Comanda C
 where C.numero not in (
 	select P.num_comanda
-    from Pedido P, Pedido_tem_Item_com_Tamanho PIT, Item_do_menu I, Categoria C 
-    where P.numero = PIT.num_pedido
-    and PIT.id_item = I.id
-    and I.categoria = C.nome
-    and C.nome != 'Cervejas Artesanais'
+	from Pedido P, Pedido_tem_Item_com_Tamanho PIT, Item_do_menu I, Categoria C 
+	where P.numero = PIT.num_pedido
+	and PIT.id_item = I.id
+	and I.categoria = C.nome
+	and C.nome != 'Cervejas Artesanais'
 );
 
 -- 4. Selecione as comandas que pediram porções mas não pediram adicionais
@@ -49,12 +49,12 @@ select C.numero
 from Comanda C
 where C.numero in (
 	select P.num_comanda
-    from Pedido P, Pedido_tem_Item_com_Tamanho PIT, Item_do_menu I, Categoria C 
-    where P.numero = PIT.num_pedido
-    and PIT.id_item = I.id
-    and I.categoria = C.nome
-    and C.nome = 'Porções'
-    and P.num_comanda not in (
+	from Pedido P, Pedido_tem_Item_com_Tamanho PIT, Item_do_menu I, Categoria C 
+	where P.numero = PIT.num_pedido
+	and PIT.id_item = I.id
+	and I.categoria = C.nome
+	and C.nome = 'Porções'
+	and P.num_comanda not in (
 		select P2.num_comanda
 		from Pedido P2, Pedido_tem_Item_com_Tamanho PIT2, Item_do_menu I2, Categoria C2
 		where P2.numero = PIT2.num_pedido
@@ -69,12 +69,12 @@ select C.numero
 from Comanda C
 where C.numero in (
 	select P.num_comanda
-    from Pedido P, Pedido_tem_Item_com_Tamanho PIT, Item_do_menu I, Categoria C 
-    where P.numero = PIT.num_pedido
-    and PIT.id_item = I.id
-    and I.categoria = C.nome
-    and C.nome = 'Porções'
-    and P.num_comanda in (
+	from Pedido P, Pedido_tem_Item_com_Tamanho PIT, Item_do_menu I, Categoria C 
+	where P.numero = PIT.num_pedido
+	and PIT.id_item = I.id
+	and I.categoria = C.nome
+	and C.nome = 'Porções'
+	and P.num_comanda in (
 		select P2.num_comanda
 		from Pedido P2, Pedido_tem_Item_com_Tamanho PIT2, Item_do_menu I2, Categoria C2
 		where P2.numero = PIT2.num_pedido
@@ -85,18 +85,18 @@ where C.numero in (
 );
 
 -- 6. Selecione o IP das impressoras que imprimiram mais de 1 item
-select inet_ntoa(I.endereco_ip)
+select inet_ntoa(I.endereco_ip) as ip
 from Impressora I, Departamento D, Categoria C
 where I.nome_depto = D.nome
 and D.responsavel_por = C.nome
 and C.nome in (
 	select C2.nome
-    from Categoria C2, Item_do_menu I, Item_com_Tamanho IT, Pedido_tem_Item_com_Tamanho PIT
-    where C2.nome = I.categoria
-    and IT.id_item = I.id
-    and PIT.id_item = IT.id_item
-    group by C2.nome
-    having count(*) > 1
+	from Categoria C2, Item_do_menu I, Item_com_Tamanho IT, Pedido_tem_Item_com_Tamanho PIT
+	where C2.nome = I.categoria
+	and IT.id_item = I.id
+	and PIT.id_item = IT.id_item
+	group by C2.nome
+	having count(*) > 1
 );
 
 -- 7. Selecione o nome dos gerentes que tem credenciais ativas e dos garçons com credenciais inativas
@@ -118,7 +118,7 @@ from Funcionario F, Funcion_gerente G
 where F.id = G.id
 and G.salario > (
 	select avg(FG.salario)
-    from Funcion_gerente FG
+	from Funcion_gerente FG
 );
 
 -- 9. Selecione o nome e o número de atendimentos do garcom que mais realizou atendimentos
@@ -126,15 +126,15 @@ and G.salario > (
 select F.nome, N.num_atendimentos
 from Funcionario F, (
 	select GAM.id_garcom as id, count(*) as num_atendimentos
-    from Garcom_atende_Mesa GAM
-    group by GAM.id_garcom
+	from Garcom_atende_Mesa GAM
+	group by GAM.id_garcom
 ) as N
 where N.id = F.id
 and N.num_atendimentos = (
 	select max(NN.Num_atendimentos) from (
 		select GAM.id_garcom as id, count(*) as num_atendimentos
 		from Garcom_atende_Mesa GAM, Funcion_garcom G
-        where G.id = GAM.id_garcom
+		where G.id = GAM.id_garcom
 		group by GAM.id_garcom
 	) as NN
 );
@@ -143,8 +143,8 @@ and N.num_atendimentos = (
 select F.nome, N.num_atendimentos*G.comissao_atend as total_comissao
 from Funcionario F, (
 	select GAM.id_garcom as id, count(*) as num_atendimentos
-    from Garcom_atende_Mesa GAM
-    group by GAM.id_garcom
+	from Garcom_atende_Mesa GAM
+	group by GAM.id_garcom
 ) as N, Funcion_garcom G
 where N.id = F.id
 and F.id = G.id;
